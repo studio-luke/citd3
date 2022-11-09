2b1dbf1ae952f80044b5bc9004ac7de1a668cc4a
MetalArmor <103912713+MetalArmor@users.noreply.github.com>
2022-11-09 13:02:01 +0900

    Add files via upload

---
+++ Bandmate/motion/__init__.py
@@ -0,0 +1,3 @@
+import motion.motion_main #import get_args, select_mode, main
+import motion.gestures.gesture_recognition
+import motion.utils.cvfpscalc
---
+++ Bandmate/motion/utils/__init__.py
@@ -0,0 +1,1 @@
+from motion.utils.cvfpscalc import CvFpsCalc
